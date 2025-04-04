a4be7e566e15d32847492f22d46bcf66208116ab
alice garcia <alice_garcia@homedepot.com>
2025-04-04 15:14:54 -0400

    refactor(headers): remove X-SPINNAKER-ACCOUNTS header

--- kork-web/src/test/java/com/netflix/spinnaker/okhttp/SpinnakerRequestInterceptorTest.java
+++ kork-web/src/test/java/com/netflix/spinnaker/okhttp/SpinnakerRequestInterceptorTest.java
@@ -22,7 +22,6 @@ import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.ok;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
-import static org.assertj.core.api.Assertions.assertThat;
 
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
@@ -32,7 +31,6 @@ import com.netflix.spinnaker.security.AuthenticatedRequest;
 import java.util.Map;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -47,18 +45,10 @@ class SpinnakerRequestInterceptorTest {
 
   public static final String TEST_USER = "some-user";
 
-  public static final String TEST_ACCOUNTS = "some-accounts";
-
   public static final String TEST_REQUEST_ID = "some-request-id";
 
   public static final Map<Header, String> TEST_SPINNAKER_HEADERS =
-      Map.of(
-          Header.USER,
-          TEST_USER,
-          Header.ACCOUNTS,
-          TEST_ACCOUNTS,
-          Header.REQUEST_ID,
-          TEST_REQUEST_ID);
+      Map.of(Header.USER, TEST_USER, Header.REQUEST_ID, TEST_REQUEST_ID);
 
   /**
    * Use this instead of annotating the class with @WireMockTest so there's a WireMock object
@@ -107,36 +97,6 @@ class SpinnakerRequestInterceptorTest {
             requestPatternBuilder.withHeader(header.getHeader(), equalTo(value));
           } else {
             requestPatternBuilder.withoutHeader(header.getHeader());
-          }
-          wireMock.verify(requestPatternBuilder);
-        });
-  }
-
-  @Test
-  void skipAccountHeaders() {
-    SpinnakerRequestInterceptor spinnakerRequestInterceptor =
-        new SpinnakerRequestInterceptor(
-            true /* propagateSpinnakerHeaders */, true /* skipAccountsHeader */);
-
-    RetrofitService retrofitService =
-        makeRetrofitService(wireMock.baseUrl(), spinnakerRequestInterceptor);
-
-    // Add some spinnaker headers to the MDC, including the accounts header
-    assertThat(TEST_SPINNAKER_HEADERS).containsKey(Header.ACCOUNTS);
-    TEST_SPINNAKER_HEADERS.forEach(AuthenticatedRequest::set);
-
-    // Make a request
-    retrofitService.getRequest();
-
-    // Verify that wiremock received all spinnaker headers except the accounts header
-    TEST_SPINNAKER_HEADERS.forEach(
-        (Header header, String value) -> {
-          RequestPatternBuilder requestPatternBuilder =
-              getRequestedFor(urlPathEqualTo(REQUEST_PATH));
-          if (Header.ACCOUNTS.equals(header)) {
-            requestPatternBuilder.withoutHeader(header.getHeader());
-          } else {
-            requestPatternBuilder.withHeader(header.getHeader(), equalTo(value));
           }
           wireMock.verify(requestPatternBuilder);
         });
